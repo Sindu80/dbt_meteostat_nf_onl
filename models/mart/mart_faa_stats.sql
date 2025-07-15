36238d954b670f9a3ff69959796bceb177c611d9
Sindu <sine.kebede@gmail.com>
2025-07-15 10:09:56 +0200

    mart_faa_stats_use.sql

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,66 +1,51 @@
 
-WITH all_flights AS (
-    SELECT
-        origin AS airport_code,
-        dest AS connected_airport,
-        tail_number,
-        airline,
-        cancelled,
-        diverted,
-        flight_date
-    FROM {{ ref('prep_flights') }}
-    
-    UNION ALL
-    
-    SELECT
-        dest AS airport_code,
-        origin AS connected_airport,
-        tail_number,
-        airline,
-        cancelled,
-        diverted,
-        flight_date
-    FROM {{ ref('prep_flights') }}
+WITH departures AS ( 
+	SELECT origin AS faa
+			,COUNT(DISTINCT dest) AS nunique_to
+			,COUNT(sched_dep_time) AS dep_planned
+			,SUM(cancelled) AS dep_cancelled
+			,SUM(diverted) AS dep_diverted
+			,COUNT(arr_time) AS dep_n_flights
+			-- ,COUNT(DISTINCT tail_number) AS dep_nunique_tails -- BONUS TASK
+			-- ,COUNT(DISTINCT airline) AS dep_nunique_airlines -- BONUS TASK
+	FROM {{ref('prep_flights')}} 
+	GROUP BY origin
+	ORDER BY origin
 ),
-
-stats_per_airport AS (
-    SELECT
-        airport_code,
-        COUNT(*) AS total_flights,
-        COUNT(*) FILTER (WHERE cancelled = 1) AS total_cancelled,
-        COUNT(*) FILTER (WHERE diverted = 1) AS total_diverted,
-        COUNT(*) FILTER (WHERE cancelled = 0 AND diverted = 0) AS total_completed,
-        COUNT(DISTINCT CASE WHEN airport_code = origin THEN dest END) AS unique_departure_connections,
-        COUNT(DISTINCT CASE WHEN airport_code = dest THEN origin END) AS unique_arrival_connections,
-        COUNT(DISTINCT tail_number) 
-            / NULLIF(COUNT(DISTINCT flight_date), 0) AS avg_unique_airplanes,
-        COUNT(DISTINCT airline) 
-            / NULLIF(COUNT(DISTINCT flight_date), 0) AS avg_unique_airlines
-    FROM {{ ref('prep_flights') }}
-    GROUP BY airport_code
+arrivals AS (
+	SELECT dest AS faa
+			,COUNT(DISTINCT origin) AS nunique_from
+			,COUNT(sched_dep_time) AS arr_planned
+			,SUM(cancelled) AS arr_cancelled
+			,SUM(diverted) AS arr_diverted
+			,COUNT(arr_time) AS arr_n_flights
+			-- ,COUNT(DISTINCT tail_number) AS arr_nunique_tails -- BONUS TASK
+			-- ,COUNT(DISTINCT airline) AS arr_nunique_airlines -- BONUS TASK
+	FROM {{ref('prep_flights')}} 
+	GROUP BY dest
+	ORDER BY dest
 ),
-
-airport_info AS (
-    SELECT
-        faa AS airport_code,
-        city,
-        country,
-        name
-    FROM {{ ref('prep_airports') }}
+total_stats AS (
+	SELECT faa
+			,nunique_to
+			,nunique_from
+	--		,(nunique_to + nunique_from)::NUMERIC/2 AS n_connections -- fractions would indicate that for the number of connections to!=from 
+			,dep_planned + arr_planned AS total_planned
+			,dep_cancelled + arr_cancelled AS total_cancelled
+			,dep_diverted + arr_diverted AS total_diverted
+	--		,((dep_cancelled + arr_cancelled + dep_diverted + arr_diverted)::NUMERIC/(dep_planned + arr_planned)::NUMERIC)*100 AS percent_change -- BONUS TASK
+			,dep_n_flights + arr_n_flights AS total_flights
+	--		,(dep_nunique_tails + arr_nunique_tails)::NUMERIC/2 AS nunique_tails -- fractions would indicate that for the number of tails to!=from -- BONUS TASK
+	--		,(dep_nunique_airlines + arr_nunique_airlines)::NUMERIC/2 AS nunique_airlines -- fractions would indicate that for the number of airlines to!=from -- BONUS TASK
+	FROM departures
+	JOIN arrivals
+	USING (faa)
 )
-
-SELECT
-    s.airport_code,
-    a.name,
-    a.city,
-    a.country,
-    s.unique_departure_connections,
-    s.unique_arrival_connections,
-    s.total_flights,
-    s.total_cancelled,
-    s.total_diverted,
-    s.total_completed,
-    s.avg_unique_airplanes,
-    s.avg_unique_airlines
-FROM stats_per_airport s
-LEFT JOIN airport_info a ON s.airport_code = a.airport_code
+SELECT a.city
+		,a.country
+		,a.name
+		, t.* 
+FROM total_stats t
+LEFT JOIN {{ref('prep_airports')}} a
+USING (faa)
+ORDER BY total_diverted DESC
